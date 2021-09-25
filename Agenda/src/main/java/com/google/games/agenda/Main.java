3747be7a188b9a5424aa3feaab16d152b01024ae
MartiPJ <61391952+MartiPJ@users.noreply.github.com>
2021-09-24 23:49:16 -0600

    Tarea de Archivos secuenciales

---
+++ Agenda/src/main/java/com/google/games/agenda/Main.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.google.games.agenda;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 
+ * @author Marti Pérez
+ */
+public class Main {
+    
+private void escribirFichero() throws IOException {
+        String nombre = "Edgar";
+        String numero = "55123412";
+        String edad = "24";
+        
+        PrintWriter out = null;
+        
+        try{
+           out = new PrintWriter(new FileWriter("archivo"));
+
+            out.printf("Nombre:%-10s Numero:%-10s Edad:%-4s", nombre, numero, edad);
+            out.println();
+            out.printf("Nombre:%-10s Numero:%-10s Edad:%-4s", "Ale", "45164908", "15");
+            out.println();
+            out.printf("Nombre:%-10s Numero:%-10s Edad:%-4s", "Alejandro", "77662314", "54");
+
+        } finally {
+            if (out!=null) {
+                out.close();
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.escribirFichero();
+    }
+
+}
